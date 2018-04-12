98716a5e27c7f52c7d60066a65706c149d626f2c
Joseph Moore <josephmoore@Josephs-MacBook-Pro.local>
2018-04-12 13:46:48 -0700

    added forloop and array practice with cards

--- src/Cards/main.go
+++ src/Cards/main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("cards main")
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
+
+	PrintArray(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
+
+//PrintArray prints each element of a string array
+func PrintArray(arr []string) {
+	//simple syntax
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+
+	//foreach syntax
+	for i, card := range arr {
+		fmt.Println(i, card)
+	}
 }
