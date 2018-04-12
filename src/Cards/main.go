f76401dbd41840389cf8885ec93978d0a873fe13
Joseph Moore <josephmoore@Josephs-MacBook-Pro.local>
2018-04-12 14:29:22 -0700

    toString() function

--- src/Cards/main.go
+++ src/Cards/main.go
@@ -6,14 +6,5 @@ import (
 
 func main() {
 	cards := newDeck()
-
-	hand, remainingDeck := deal(cards, 5)
-
-	fmt.Println("hand")
-	hand.print()
-
-	fmt.Println()
-
-	fmt.Println("remaining Deck")
-	remainingDeck.print()
+	fmt.Println(cards.toString())
 }
