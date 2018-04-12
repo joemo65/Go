e0129e0b31620b72b6884cfcc58ee933ee1df503
Joseph Moore <josephmoore@Josephs-MacBook-Pro.local>
2018-04-12 12:17:24 -0700

    added switch statement practice

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,10 +5,12 @@ import "fmt"
 func main() {
 	fmt.Printf("hello, world this is GO\n")
 
+	fmt.Println("for loop practice")
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
 
+	fmt.Println("conditional statement practice")
 	age := 39
 
 	if age < 13 {
@@ -25,4 +27,24 @@ func main() {
 		fmt.Println("Over the hill!")
 	}
 
+	fmt.Println("switch statement practice")
+	day := 0
+	switch day {
+	case 0:
+		fmt.Println("sunday")
+	case 1:
+		fmt.Println("monday")
+	case 2:
+		fmt.Println("tuesday")
+	case 3:
+		fmt.Println("wednesday")
+	case 4:
+		fmt.Println("thursday")
+	case 5:
+		fmt.Println("friday")
+	case 6:
+		fmt.Println("saturday")
+	default:
+		fmt.Println("it's groundhog day")
+	}
 }
