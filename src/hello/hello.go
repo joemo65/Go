b0534a84906804c70ae394f58e3413cb7dc5230a
Joseph Moore <josephmoore@Josephs-MacBook-Pro.local>
2018-04-12 12:10:35 -0700

    added else if statements

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -9,24 +9,19 @@ func main() {
 		fmt.Println(i)
 	}
 
-	age := 12
+	age := 39
 
 	if age < 13 {
 		fmt.Println("Wow you're young!")
-	}
-	if age > 12 && age < 20 {
+	} else if age < 20 {
 		fmt.Println("You're a teenager")
-	}
-	if age > 19 && age < 30 {
+	} else if age < 30 {
 		fmt.Println("You're in your twenties")
-	}
-	if age > 29 && age < 40 {
+	} else if age < 40 {
 		fmt.Println("you're in your thirties")
-	}
-	if age > 39 && age < 50 {
+	} else if age < 50 {
 		fmt.Println("You're getting there!")
-	}
-	if age > 49 {
+	} else {
 		fmt.Println("Over the hill!")
 	}
 
