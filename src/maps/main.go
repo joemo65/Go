4fb7149c21478b178ceeffd5bfeb18bb83c1c9d0
Joseph Moore <josephmoore@Josephs-MacBook-Pro.local>
2018-04-12 16:20:18 -0700

    maps stuff done

--- src/maps/main.go
+++ src/maps/main.go
@@ -5,14 +5,17 @@ import (
 )
 
 func main() {
-	colors := make(map[string]string)
-	colors["white"] = "#ffffff"
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	//delete(colors, "white")
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	printMap(colors)
+}
 
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
